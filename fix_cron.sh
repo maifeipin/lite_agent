3842c7d849319a78488a1626979b92b535ac060a
maifeipin <maifeipin@users.noreply.github.com>
2026-06-21 11:11:11 +0800

    chore: cleanup remaining temporary test scripts

--- fix_cron.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-(crontab -l 2>/dev/null | grep -v edge_sentinel.py; echo "*/5 * * * * python3 /opt/edge_sentinel/edge_sentinel.py") | crontab -
--- test_config.py
+++
@@ -1,4 +0,0 @@
-﻿import os, sys
-sys.path.insert(0, '/home/liteagent/lite_agent')
-from config_loader import load_config
-print("TOKEN IS:", load_config().get("edge_token"))
